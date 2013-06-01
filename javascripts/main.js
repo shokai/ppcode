7ca15cc8ce08e2dc6a705441dc64e37c5b034fc7
Sho Hashimoto <hashimoto@shokai.org>
2013-06-01 16:13:37 +0900

    incremental prettify

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+$(function(){
+  $("#code").keyup(function(e){
+    console.log("pretty");
+    var code = $("#code").val();
+    var pre = $("<pre>").text(code).addClass("prettyprint");
+    $("#prettyprint").html(pre);
+    $("#script").html( $("<script>").attr("src", "./javascripts/run_prettify.js") );
+  });
+});
+
