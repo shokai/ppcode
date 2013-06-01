2e97b7dd740b0ea0dfa8e1bde994260594b30229
Sho Hashimoto <hashimoto@shokai.org>
2013-06-01 16:37:40 +0900

    speedup

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,12 +1,16 @@
+var eid = null;
 var prettify = function(){
+  if(eid) clearTimeout(eid);
+  eid = setTimeout(function(){
+    eid = null;
     var code = $("#code").val();
     var pre = $("<pre>").text(code).addClass("prettyprint");
     $("#prettyprint").html(pre);
     $("#script").html( $("<script>").attr("src", "./javascripts/run_prettify.js") );
+  }, 400);
 };
 
 $(function(){
   prettify();
   $("#code").keyup(prettify);
 });
-
