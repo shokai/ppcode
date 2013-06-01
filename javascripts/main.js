06eddd46ad6e0d7d24599fbc56775808df880b7e
Sho Hashimoto <hashimoto@shokai.org>
2013-06-01 16:15:05 +0900

    prettify on page load

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,12 @@
-$(function(){
-  $("#code").keyup(function(e){
-    console.log("pretty");
+var prettify = function(){
     var code = $("#code").val();
     var pre = $("<pre>").text(code).addClass("prettyprint");
     $("#prettyprint").html(pre);
     $("#script").html( $("<script>").attr("src", "./javascripts/run_prettify.js") );
-  });
+};
+
+$(function(){
+  prettify();
+  $("#code").keyup(prettify);
 });
 
